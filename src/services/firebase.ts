3e48cfa5b12eaabf9e473c62954fda6195990e1a
4ntti13 <102434268+4ntti13@users.noreply.github.com>
2024-01-03 18:25:26 +0200

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,7 @@ import firebase from '@react-native-firebase/app';
 import auth from '@react-native-firebase/auth';
 
 const firebaseConfig = {
-    apiKey:"",
-    authDomain: 'timeandturfapp.firebaseapp.com',
-    projectId: 'timeandturfapp',
-    storageBucket: 'timeandturfapp.appspot.com',
-    messagingSenderId: '1091743621537',
-    appId: '1:1091743621537:web:a16994886b609a1b2f6be0',
-    measurementId: 'G-3CFV8BVTM4',
+// apikey and stuff
   };
 
 if (!firebase.apps.length) {
